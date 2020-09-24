e7af39772595a79db0615e7c7ebb6fc44a548b90
HuangRihChang <it.hrchang@gmail.com>
2020-09-24 11:04:20 +0700

    remove python buffer

--- setup-dataset.sh
+++ setup-dataset.sh
@@ -13,20 +13,20 @@ export SHELL=/bin/bash
 # common="jbsub -mem 64g -cores 1 -queue x86_1h -proj $proj"
 
 # some instances clearly does not have 50000 states/transitiosn, but lets forget about it for now
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mnist 3 3 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mnist 4 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mandrill 3 3 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mandrill 4 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle spider 3 3 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle spider 4 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout digital 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout digital 5 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout twisted 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout twisted 5 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 3 3 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 4 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 9 3 50000
-PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 8 50000
+./setup-dataset.py puzzle mnist 3 3 50000
+./setup-dataset.py puzzle mnist 4 4 50000
+./setup-dataset.py puzzle mandrill 3 3 50000
+./setup-dataset.py puzzle mandrill 4 4 50000
+./setup-dataset.py puzzle spider 3 3 50000
+./setup-dataset.py puzzle spider 4 4 50000
+./setup-dataset.py lightsout digital 4 50000
+./setup-dataset.py lightsout digital 5 50000
+./setup-dataset.py lightsout twisted 4 50000
+./setup-dataset.py lightsout twisted 5 50000
+./setup-dataset.py hanoi 3 3 50000
+./setup-dataset.py hanoi 4 4 50000
+./setup-dataset.py hanoi 9 3 50000
+./setup-dataset.py hanoi 4 8 50000
 
 download-and-extract (){
     wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1.npz -O latplan/puzzles/$1.npz
@@ -36,10 +36,10 @@ download-and-extract (){
 
 export -f download-and-extract
 
-PYTHONUNBUFFERED=1 download-and-extract blocks-5-3
-PYTHONUNBUFFERED=1 download-and-extract blocks-4-4
-PYTHONUNBUFFERED=1 download-and-extract blocks-3-7
-PYTHONUNBUFFERED=1 download-and-extract blocks-3-6
-PYTHONUNBUFFERED=1 download-and-extract blocks-3-5
-PYTHONUNBUFFERED=1 download-and-extract blocks-3-4
-PYTHONUNBUFFERED=1 download-and-extract blocks-3-3
+download-and-extract blocks-5-3
+download-and-extract blocks-4-4
+download-and-extract blocks-3-7
+download-and-extract blocks-3-6
+download-and-extract blocks-3-5
+download-and-extract blocks-3-4
+download-and-extract blocks-3-3
